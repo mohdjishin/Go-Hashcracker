a14cc9c2afc2372d076f6253a4d23cd7d2412e14
mohdjishin <whoistcp@protonmail.com>
2023-04-04 02:34:59 +0530

    command line args

--- main.go
+++ main.go
@@ -4,18 +4,25 @@ import (
 	"bufio"
 	"crypto/sha256"
 	"encoding/hex"
+	"flag"
 	"fmt"
 	"os"
 	"runtime"
+	"strings"
 	"sync"
 )
 
-func generateHash(text string) string {
-	hash := sha256.Sum256(([]byte(text)))
+func generateHash(text string, hashType string) string {
+	var hash [32]byte
+	if hashType == "sha256" {
+		hash = sha256.Sum256(([]byte(text)))
+	} else {
+		panic("Unsupported hash type")
+	}
 	return hex.EncodeToString(hash[:])
 }
 
-func crackHash(hash string, wordlistPath string, maxWorkers int) string {
+func crackHash(hash string, wordlistPath string, maxWorkers int, hashType string) string {
 	file, err := os.Open(wordlistPath)
 	if err != nil {
 		panic(err)
@@ -33,7 +40,7 @@ func crackHash(hash string, wordlistPath string, maxWorkers int) string {
 		go func() {
 			defer wg.Done()
 			for word := range jobs {
-				if generateHash(word) == hash {
+				if generateHash(word, hashType) == hash {
 					fmt.Printf("Password found: %s\n", word)
 					return
 				}
@@ -52,10 +59,21 @@ func crackHash(hash string, wordlistPath string, maxWorkers int) string {
 }
 
 func main() {
-	hashToCrack := "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8"
-	wordlistPath := "wordlist.txt"
-	maxWorkers := runtime.NumCPU()
+	var maxWorkers *int
 
-	result := crackHash(hashToCrack, wordlistPath, maxWorkers)
-	fmt.Printf("Hash: %s\nPassword: %s\n", hashToCrack, result)
+	hashType := flag.String("type", "sha256", "Hash type (sha256 only supported)")
+	hashValue := flag.String("hash", "", "Hash value to crack")
+	wordlistPath := flag.String("wordlist", "wordlist.txt", "Path to wordlist")
+	maxWorkers = flag.Int("workers", runtime.NumCPU(), "Number of workers to use")
+	flag.Parse()
+
+	if *hashValue == "" {
+		fmt.Println("Please provide a hash value to crack using the -hash flag")
+		return
+	}
+
+	result := crackHash(strings.TrimSpace(*hashValue), *wordlistPath, *maxWorkers, *hashType)
+	fmt.Printf("Hash: %s\nPassword: %s\n", *hashValue, result)
 }
+
+// useage:= go run main.go -type=sha256 -hash=5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8 -wordlist=wordlist.txt -workers=10
